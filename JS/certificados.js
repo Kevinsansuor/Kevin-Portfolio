a888e43c91b23c04a4665d676d5d4611fc6fd823
unknown <kevinsansuor@gmail.com>
2023-07-22 22:02:48 -0500

    logica botones certificados

---
+++ JS/certificados.js
@@ -0,0 +1,29 @@
+let certificados_googleactive = document.querySelectorAll('#d8e660192df340f39601247cfa6160ae');
+
+certificados_googleactive.forEach(item => item.onclick= ()=>{
+    window.open('https://learndigital.withgoogle.com/activate/validate-certificate-code');
+})
+
+let certificado_Formación_Desarrollo_Personal_G3_ONE = document.querySelector('#cb869b9d4d58434d9f7333aa1cdd3ed0');
+
+certificado_Formación_Desarrollo_Personal_G3_ONE.onclick=()=>{
+    window.open('https://app.aluracursos.com/degree/certificate/e3f48591-28cf-412e-b959-c1fc267cdb04')
+};
+
+let certificado_Formación_Front_End_G3_ONE = document.querySelector('#e491323de0ea4a81a6787d31c5ab4be4');
+
+certificado_Formación_Front_End_G3_ONE.onclick=()=>{
+    window.open('https://app.aluracursos.com/degree/certificate/1c87e726-9464-4b74-b1ec-b8e9e25e9c62');
+};
+
+let certificado_Formación_Principiante_en_Programación_G3_ONE = document.querySelector('#a95b5ca3138e433b8c1b01a6fc21b879');
+
+certificado_Formación_Principiante_en_Programación_G3_ONE.onclick=()=>{
+    window.open('https://app.aluracursos.com/degree/certificate/2496a017-fcd0-4f21-a952-962373648004');
+};
+
+let certificado_EF_Standard_English_Test = document.querySelector('#f8d67ae947e24e2d818a0d2c58ff24d7');
+
+certificado_EF_Standard_English_Test.onclick=()=>{
+    window.open('https://www.efset.org/cert/oF3Dsi');
+};
